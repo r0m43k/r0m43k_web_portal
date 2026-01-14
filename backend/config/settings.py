552047cf7ba2c413db8fe468b98c8c1745efb2cf
r0m43k <r0m43k@kali.org>
2026-01-14 15:39:42 -0500

    updated

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -88,6 +88,7 @@ SESSION_COOKIE_HTTPONLY = True
 CSRF_COOKIE_HTTPONLY = False
 SESSION_COOKIE_SAMESITE = "Lax"  # "Strict" if ENV == "prod" else "Lax"
 CSRF_COOKIE_SAMESITE = "Lax"  # "Strict" if ENV == "prod" else "Lax"
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
