d427ecb2d95ed680f1cb8eacfb4814756a830b09
r0m43k <r0m43k@mail.ru>
2026-01-15 16:55:38 +0500

    update

---
+++ backend/managment/commands/createsu.py
@@ -0,0 +1,72 @@
+import os
+
+from django.contrib.auth import get_user_model
+
+from django.core.management.base import BaseCommand
+
+
+User = get_user_model()
+
+class Command(BaseCommand):
+
+
+    help = "Create superuser from env vars"
+
+
+
+
+
+    def handle(self, *args, **options):
+
+
+        username = os.getenv("SUPERUSER_USERNAME")
+
+
+        password = os.getenv("SUPERUSER_PASSWORD")
+
+
+        email = os.getenv("SUPERUSER_EMAIL", "")
+
+
+
+
+
+        if not username or not password:
+
+
+            self.stdout.write("Superuser env vars not set, skipping")
+
+
+            return
+
+
+
+
+
+        if User.objects.filter(username=username).exists():
+
+
+            self.stdout.write("Superuser already exists")
+
+
+            return
+
+
+
+
+
+        User.objects.create_superuser(
+
+
+            username=username,
+
+
+            password=password,
+
+
+            email=email,
+
+
+        )
+
+
